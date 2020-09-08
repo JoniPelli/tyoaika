eb7c1fa3aea72de150f98ec7f6a553923b4e180f
JoniPelli <jonipelli@gmail.com>
2020-09-08 13:50:49 +0300

    ---

--- työaika/MainWindow.xaml.cs
+++ työaika/MainWindow.xaml.cs
@@ -58,7 +58,7 @@ namespace työaika
         private void btnTehtavaPoista_Click(object sender, RoutedEventArgs e)
         {
             //Marjolle hommia
-            //Jonille kans muutama
+            //Sarille kanssa
         }
 
         private void btnKohdeLisaa_Click(object sender, RoutedEventArgs e)
